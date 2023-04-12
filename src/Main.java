f4ad7b99ecb92ffb8b6b93122a1805f8603bd2f7
ПК2 <alina.erdeshova03@gmail.com>
2023-04-12 21:01:40 +0300

    n

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scan =new Scanner(System.in);
+        int a = scan.nextInt();
+        int b= scan.nextInt();
+        int c= scan.nextInt();
+
+        int summa=a+b+c;
+        System.out.println(summa);
+        int koboitundusu=a*b*c;
+        System.out.println(koboitundusu);
+        double arif=summa/3;
+        System.out.println(arif);
+
+//        int a = 5;
+//
+//        a = a + 1;
+//        int v1 = a;
+//
+//        a = a + 1;
+//        int v2 = a;
+//
+//        int b = v1 + v2;
+//        System.out.println(b);
+
+        //туура же ката екенин текшерет
+//        System.out.println(s.endsWith("na"));
+//        //башындагыны  текшерет
+//        System.out.println(s.startsWith("al"));
+//        //индексн берет
+//        System.out.println(s.indexOf("A"));
+
+
+
+//        System.out.println(methood(1,2,5,6));
+//       soz();
+//       String name="Alina";
+//       //бир символду кайтарат
+//        System.out.println(name.charAt(4));
+//        //бир болугун кыркып берет
+//        System.out.println(name.substring(3,7));
+//        //камтыйт же камтыбайт текшерип берет
+//        System.out.println(name.contains("name"));
+//        if(name.contains("s")){
+//            System.out.println("камтыйт");
+//        }else {
+//            System.out. println("jok");
+//            //бардагын чон тамга мн чыгарат
+//            System.out.println(name.toUpperCase(Locale.ROOT));
+//            //башындагы аягындагы прабелдерди очурот
+//            System.out.println(name.trim());
+//            //
+//
+//            if (name.equals("Alina")){
+//                System.out.println("barabar");
+//            }else{
+//                System.out.println("barabar emes");
+//                //
+//
+//            }
+//        }
+//    }
+//    static int methood(int...a) {
+//        int sum = 0;
+//        for (int i = 0; i < a.length; i++) {
+//
+//            sum = sum + a[i];
+//
+//        }
+//        return sum;
+//
+//    }
+//    static void soz(){
+//        String[] a = new String[]{"Alina", "Asel", "Aidai"};
+//        for (String s : a) {
+//            System.out.println("I love you " + s);
+//        }
+//    }
+    }
+
+
+//    static void slova(String s) {
+//        Scanner scanner = new Scanner(System.in);
+//        String s1 = scanner.nextLine();
+//        System.out.println(s1.toUpperCase(Locale.ROOT));
+//        System.out.println(s1.trim());
+//        System.out.println(s1.contains(s1));
+//        if (s1.contains("A")) {
+//            System.out.println(s1.substring(2, 5));
+//        } else {
+//            System.out.println("жок");
+
+        }
+
+
+
+
+
+
+
